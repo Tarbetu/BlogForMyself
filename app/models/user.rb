1e595debf73f61ab9701b25fe5cb3b7f4fd82b57
Emrecan Şuşter <emrecansuster@tuta.io>
2021-12-11 17:41:15 +0300

    adding views and blocking signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# This model is managed by Clearance
+# Users should only created by console and this user must confirmed automatically.
 class User < ApplicationRecord
   include Clearance::User
+
+  after_create :confirm_user
+
+  def send_confirmation_email; end
+
+  private
+
+  def confirm_user
+    confirm_email!
+  end
 end
--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 Clearance.configure do |config|
-  config.mailer_sender = "reply@example.com"
+  config.mailer_sender = "no_ones@mail.com"
   config.rotate_csrf_on_sign_in = true
+  config.allow_sign_up = false
 end
