ae50ec594f0a60969879b19178ea40b6742726ac
Emrecan Şuşter <emrecansuster@tuta.io>
2021-12-13 22:58:13 +0300

    preparing for deploy

--- Gemfile
+++ Gemfile
@@ -68,8 +68,8 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-
 gem "clearance"
 gem "image_processing", "~> 1.12"
 
-gem "mina", "~> 1.2"
+gem "mina", "~> 1.2", require: false
+gem "mina-puma", require: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+# "Hakkında" means "About" in Turkish. This category hard-coded parts of this project.
+# I know that this is a bad practice but I'm too lazy to fix it.
+Category.create!(name: "Hakkında")
