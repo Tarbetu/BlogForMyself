71a93dec498a1f309cda0973b32d7f568c87c77f
Emrecan Şuşter <emrecansuster@tuta.io>
2022-02-11 10:49:31 +0300

    categories fixed

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,5 +3,5 @@
 # This model contains posts, also can be used for static pages
 class Category < ApplicationRecord
   has_many :posts
-  validates :name, length: { minimum: 5 }
+  validates :name, length: { in: 5..20 }
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,28 @@
+# frozen_string_literal: true
+
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the name of category shall exist" do
+    new_category = Category.new
+    assert_not new_category.save, "Saving a category without name"
+  end
+
+  test "the name of category shall be longer than 5" do
+    new_category = Category.new
+    new_category.name = "cik"
+    assert_not new_category.save, "Saving a category with a silly name"
+  end
+
+  test "the name of category shall be shorter than 20 chars" do
+    new_category = Category.new
+    new_category.name = "Thine hope, thine suffer and my eternal emotions when I missed you"
+    assert_not new_category.save, "Trying to save a Gothic Metal band name as a Category name"
+  end
+
+  test "the category shall be saved if there is valid name" do
+    new_category = Category.new
+    new_category.name = "Cuties"
+    assert new_category.save, "Don't save the valid category"
+  end
 end
