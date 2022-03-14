e2274c375f63cf4e41a10742c3a834952e48e4c9
Emrecan Şuşter <emrecansuster@tuta.io>
2022-03-15 00:23:25 +0300

    fixing preview images and listing in main page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,13 +8,13 @@ class CategoriesController < ApplicationController
 
   # GET /categories or /categories.json
   def index
-    @pagy, @posts = pagy Post.limit(3)
-    @category_pagy, @categories = pagy Category.where.not(name: "Hakkında")
+    @pagy, @posts = pagy Post.limit(3).reverse_order
+    @category_pagy, @categories = pagy Category.where.not(name: 'Hakkında')
   end
 
   # GET /categories/1 or /categories/1.json
   def show
-    @pagy, @category_posts = pagy @category.posts.order("created_at DESC")
+    @pagy, @category_posts = pagy @category.posts.order('created_at DESC')
 
     # This part is just copy-paste from posts#index
     respond_to do |format|
@@ -46,7 +46,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to @category, notice: "Category was successfully created." }
+        format.html { redirect_to @category, notice: 'Category was successfully created.' }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -59,7 +59,7 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to @category, notice: "Category was successfully updated." }
+        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
         format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -72,19 +72,20 @@ class CategoriesController < ApplicationController
   def destroy
     @category.destroy
     respond_to do |format|
-      format.html { redirect_to categories_url, notice: "Category was successfully destroyed." }
+      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_category
-      @category = Category.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def category_params
-      params.require(:category).permit(:name)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
