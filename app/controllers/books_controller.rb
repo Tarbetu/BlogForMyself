95569612decd6f17a6f7f361ee15c20e14a652ba
Emrecan Şuşter <emrecansuster@tuta.io>
2022-04-26 23:13:04 +0300

    fixing controller logic and views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -48,7 +48,7 @@ class BooksController < ApplicationController
 
   private
     def set_book
-      @book = Book.find(params[:id])
+      @book = Book.find_by(name: params[:name])
     end
 
     def book_params
